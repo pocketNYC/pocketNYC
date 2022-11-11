cb1a0a5e24062cffce8659477ddbefb94fd46cc6
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-10 21:18:45 -0500

    edited tags display

--- client/features/resources/SingleResource.js
+++ client/features/resources/SingleResource.js
@@ -14,20 +14,27 @@ const SingleResource = () => {
   const { name, description, address, tag, boro, hyperlink, imageUrl } =
     resource;
 
+  const modifyCasing = name
+    ?.split(" ")
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    })
+    .join(" ");
+
   useEffect(() => {
     dispatch(fetchSingleResource(id));
   }, [dispatch]);
 
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{modifyCasing}</h1>
       <img src={imageUrl} />
       <p>About: {description}</p>
       <p>Address: {address}</p>
       <p>
         More Info: <a href={hyperlink}>{hyperlink}</a>
       </p>
-      <p>Tags: {tag}</p>
+      <p>Tags: {tag ? tag.join(", ") : null}</p>
     </div>
   );
 };
