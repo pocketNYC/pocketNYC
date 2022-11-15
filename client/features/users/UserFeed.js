afc1d8419e1abb7b1fd32a288b8eb715d38b170a
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-15 14:53:00 -0500

    modified events seed file to turn interests into multiple strings

--- client/features/users/UserFeed.js
+++ client/features/users/UserFeed.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchAllEvents } from "../events/eventsSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { addToFavEvent } from "../favorites/favoriteEventSlice";
+import { Button } from "react-bootstrap";
 
 function UserFeed({ interests, borough }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllEvents());
+  }, [dispatch]);
+
+  const events = useSelector((state) => state.events.events);
+  const addButton = (ev, id) => {
+    dispatch(addToFavEvent(id));
+  };
+  const filteredByBoro = events.filter((event) => {
+    if (event.status === "approved" && event.borough === borough) {
+      return event;
+    }
+  });
+
+  const filteredByInterest = filteredByBoro.filter((event) => {
+    for (let i = 0; i < event.tag.length; i++) {
+      console.log(event.tag, "event.tag");
+      let tag = event.tag[i];
+      console.log("tag", tag);
+      if (interests.includes(tag)) {
+        return event;
+      }
+    }
+  });
+
+  console.log("ALL EVENTS", events);
+  console.log("F EVENTS", filteredByBoro);
+  console.log("interests", interests);
+  console.log("F INTEREST", filteredByInterest);
 
   //user feed: APPROVED events, in my BOROUGH, matching my INTERESTS
   return (
@@ -9,6 +43,27 @@ function UserFeed({ interests, borough }) {
       <p>
         I'm interested in {interests} events happening in {borough}
       </p>
+      <p>Events matching your interests & borough:</p>
+      {filteredByInterest
+        ? filteredByInterest?.map(({ id, image, title, date }) => (
+            <div key={id}>
+              <img
+                src={image}
+                style={{ width: "500px", height: "300px" }}
+                onClick={() => navigate(`/events/${id}`)}
+              />
+              <h3 className="underline">{title}</h3>
+              <h4>
+                Date: {date}
+                <br />
+                <br />
+                <Button variant="primary" onClick={(ev) => addButton(ev, id)}>
+                  Add to Favorites
+                </Button>
+              </h4>
+            </div>
+          ))
+        : "No events matching your interests/borough."}
     </div>
   );
 }
