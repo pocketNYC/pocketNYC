ed1aa74d51c48a7b67c804912fd0d318f24624a9
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-15 16:31:06 -0500

    edit event slice, thunk, and button working

--- client/features/admin/Admin.js
+++ client/features/admin/Admin.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { editEvent, fetchAllEvents } from "../events/eventsSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function Admin() {
-  const { firstName } = useSelector((state) => state.auth.me);
+  const dispatch = useDispatch();
+  const events = useSelector((state) => state.events.events);
+
+  useEffect(() => {
+    dispatch(fetchAllEvents());
+  }, [dispatch]);
+
+  const pendingEvents = events?.filter((event) => {
+    if (event.status === "pending") {
+      return event;
+    }
+  });
+
+  const editEventButton = (ev, id) => {
+    dispatch(editEvent(id, ev));
+  };
 
   return (
     <div>
       <br />
-      <h2>
-        Pending Events Here
-        <hr />
-        Edit Admin Profile
-        <hr />
-        Edit a User Here
-      </h2>
+      <h2>Pending Events Here</h2>
+      <>
+        <ul>
+          {pendingEvents.map(({ title, id, date, status }) => {
+            return (
+              <li key={id}>
+                <small>{title}</small>
+                <br />
+                <small>{date}</small>
+                <br />
+                <button onClick={(ev) => editEventButton(ev, id)}>
+                  Approve
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+
+      <hr />
+      <h2> Edit Admin Profile</h2>
+      <hr />
+      <h2>Edit a User Here</h2>
     </div>
   );
 }
