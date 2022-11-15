660bb79e842cc8ca08d0201610fce67f84bfe41f
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-14 20:08:47 -0500

    incorporated my favorites to user profile

--- client/features/users/SingleUserProfile.js
+++ client/features/users/SingleUserProfile.js
@@ -2,17 +2,22 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleUser } from "./userSlice";
 import { Link } from "react-router-dom";
+import Favorites from "../favorites/Favorites";
+import { fetchFavoriteEvent } from "../favorites/favoriteEventSlice";
+import { fetchFavoriteResource } from "../favorites/favoriteResourceSlice";
 
 function SingleUserProfile() {
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.auth.me.id);
 
-  const { firstName, lastName, email, interests, borough } = useSelector(
+  const { firstName, lastName, email, interests, borough, id } = useSelector(
     (state) => state.user.singleUser
   );
 
   useEffect(() => {
     dispatch(fetchSingleUser(userId));
+    dispatch(fetchFavoriteEvent());
+    dispatch(fetchFavoriteResource());
   }, [dispatch]);
 
   return (
@@ -29,6 +34,7 @@ function SingleUserProfile() {
         <strong>Borough: </strong>
         {borough}
       </h6>
+      <Link to={`/users/${id}/favorites`}>My Favorites</Link>
     </div>
   );
 }
