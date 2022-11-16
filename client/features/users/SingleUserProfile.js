49f58c684e60e97b561d25e8f5b3100dd0c35b90
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-16 15:17:57 -0500

    implemented dropdown for user profile

--- client/features/users/SingleUserProfile.js
+++ client/features/users/SingleUserProfile.js
@@ -26,93 +26,82 @@ function SingleUserProfile() {
       return event;
     }
   });
-  console.log(myEvents, "MYEVTS");
 
   return (
     <div>
-      <h4> {`${firstName} ${lastName}`} </h4>
-      <h6>
-        <strong>Email:</strong> {email}
-      </h6>
-      <h6>
-        <strong>Interests: </strong>
-        {[interests].toString().split(",").join(", ")}
-      </h6>
-      <h6>
-        <strong>Borough: </strong>
-        {borough}
-      </h6>
-      <div className="accordion" id="accordionExample">
-        <div className="card">
-          <div class="card-header" id="headingOne">
-            <h2 class="mb-0">
-              <button
-                class="btn btn-link"
-                type="button"
-                data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                My Events Submitted
-              </button>
-            </h2>
-          </div>
-          <div
-            id="collapseOne"
-            className="collapse show"
-            aria-labelledby="headingOne"
-            data-parent="#accordionExample"
-          >
-            <div className="card-body">
-              <ul>
-                {myEvents.map(({ title, status, id, date }) => {
-                  return (
-                    <li key={id}>
-                      {title}
-                      <ul>
-                        <li> Date: {date}</li>
-                        <li> Status: {status}</li>
-                      </ul>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-header" id="headingTwo">
-              <h2 class="mb-0">
-                <button
-                  class="btn btn-link collapsed"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  Collapsible Group Item #2
-                </button>
-              </h2>
-            </div>
-            <div
-              id="collapseTwo"
-              class="collapse"
-              aria-labelledby="headingTwo"
-              data-parent="#accordionExample"
+      <h4> Welcome, {`${firstName} ${lastName}`}! </h4>
+
+      <div className="accordion accordion-flush" id="accordionFlushExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
             >
-              <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </div>
+              My User Information
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">
+              <h6>
+                <strong>Email:</strong> {email}
+              </h6>
+              <h6>
+                <strong>Interests: </strong>
+                {[interests].toString().split(",").join(", ")}
+              </h6>
+              <h6>
+                <strong>Borough: </strong>
+                {borough}
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingOne">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              My Submitted Events
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">
+              <ul>
+                {myEvents.length
+                  ? myEvents.map(({ title, status, id, date }) => {
+                      return (
+                        <li key={id}>
+                          {title}
+                          <ul>
+                            <li> Date: {date}</li>
+                            <li> Status: {status}</li>
+                          </ul>
+                        </li>
+                      );
+                    })
+                  : "No events to display"}
+              </ul>
             </div>
           </div>
         </div>
