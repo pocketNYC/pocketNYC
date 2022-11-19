5601c27fa771170fe5c2e6a6225948dfd1bbf06e
Noor Mudassar <noormudassar97@gmail.com>
2022-11-18 19:28:52 -0500

    map functioning on page

--- client/features/map/Map.js
+++ client/features/map/Map.js
@@ -1,23 +1,30 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
-import PinDropIcon from "@mui/icons-material/PinDrop";
+import PushPinRoundedIcon from "@mui/icons-material/PushPinRounded";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 const Map = () => {
   return (
-    <div id="map" className="map, underline">
+    <div id="map" className="map, underline, center">
       <h1 align="center">Map</h1>
-      <MapContainer center={[40.712, -74.0]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={[40.6782, -73.9442]}
+        zoom={12}
+        scrollWheelZoom={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <PinDropIcon position={[40.748, -73.985]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </PinDropIcon>
+        <div>
+          <Marker position={[40.574879, -73.982872]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </div>
       </MapContainer>
+      <PushPinRoundedIcon />
     </div>
   );
 };
