f3b4ef2a2aa55087ea31b2665c6a9484548436f5
Breana Johnson <brecjohnson@gmail.com>
2022-11-21 17:16:37 -0500

    minor CSS update to optimize rendering of users for Admin view on mobile

---
+++ client/features/admin/UserTable.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllUsers } from "../users/userSlice";
+import Toggle from "react-toggle";
+import "./toggleButton.css";
+import { editUser } from "../users/userSlice";
+import "./userTable.css";
+
+function UserTable() {
+  const users = useSelector((state) => state.user.allUsers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllUsers());
+  }, [dispatch]);
+
+  return (
+    <div className="userTable">
+      <table class="table table-hover">
+        <thead>
+          <tr className="table-headers-user-dash">
+            {/* <th>First Name</th>
+            <th>Last Name</th> */}
+            <th className="user-containter">User</th>
+            <th className="admin-priv-container">Administrator Privileges</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map(
+            ({
+              id,
+              firstName,
+              lastName,
+              email,
+              borough,
+              interests,
+              isAdmin,
+            }) => {
+              return (
+                <tr key={id} className="user-info-dashboard-table">
+                  <td className="user-dash-userInfo">
+                    <div className="user-text">
+                      <strong>Name:</strong> {`${firstName} ${lastName}`} <br />
+                      <strong>Email:</strong> {email}
+                    </div>
+                  </td>
+                  <td>
+                    <label className="toggle-label">
+                      <Toggle
+                        defaultChecked={isAdmin}
+                        onClick={() => {
+                          isAdmin ? (isAdmin = false) : (isAdmin = true);
+                          dispatch(
+                            editUser({
+                              id,
+                              firstName,
+                              lastName,
+                              email,
+                              borough,
+                              interests,
+                              isAdmin,
+                            }),
+                            [dispatch]
+                          );
+                        }}
+                      />
+                    </label>
+                  </td>
+                </tr>
+              );
+            }
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UserTable;
