588fe8f5143ceb87beac1b1d1f89d3dd0de36bd6
Noor Mudassar <noormudassar97@gmail.com>
2022-11-22 10:27:43 -0500

    added map page

--- client/features/map/Map.js
+++ client/features/map/Map.js
@@ -1,18 +1,39 @@
 import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { icon } from "leaflet";
+
+const ICON = icon({
+  iconUrl: "/geo-fill.svg",
+  iconSize: [29, 29],
+});
 
 function Map() {
   return (
-    <div>
-      <center>
-        <img
-          src={
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.google.com%2Fmaps%2Fd%2Fthumbnail%3Fmid%3D19tYGrxvVWr345v_VePrUfooqtmo&f=1&nofb=1&ipt=55786a1341a7757360a6208088b7dacc1992abc05dd7c10792c7d87dc8289dcd&ipo=images"
-          }
-          alt={"image of a map"}
-        />
-        <h2>Pin your selection of Events and Resources on our map!</h2>
-        <h2>Coming Soon...</h2>
-      </center>
+    <div id="map" className="map, underline">
+      <h1 align="center">Map</h1>
+      <div className="d-flex justify-content-center">
+        <MapContainer
+          center={[40.6782, -73.9442]}
+          zoom={11}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <div>
+            <Marker
+              icon={ICON}
+              position={[40.70296058477033, -74.01551608783149]}
+            >
+              <Popup>
+                A testing popup. <br /> This is The Battery Park.
+              </Popup>
+            </Marker>
+          </div>
+        </MapContainer>
+      </div>
     </div>
   );
 }
