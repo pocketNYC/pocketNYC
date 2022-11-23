63db9aa718f47de81b3b664b560a2b64f75fe6c6
Breana Johnson <brecjohnson@gmail.com>
2022-11-23 01:36:11 -0500

    loading Users (Admin view) page

--- client/features/loading/LoadingScreen.js
+++ client/features/loading/LoadingScreen.js
@@ -3,7 +3,7 @@ import './LoadingScreen.css'
 function LoadingScreen() {
   return (
     <div>
-      <h1 style={{textAlign:'center'}}>This page will load in a New York minute!</h1>
+      <h1 style={{textAlign:'center', color:'red'}}>This page will load in a New York minute!</h1>
       <img src="pocketlogo.png" alt='' />
     </div>
   );
