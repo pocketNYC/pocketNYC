2c0f88b55332ecc8b8286875cff928d09bcdbaa9
Breana Johnson <brecjohnson@gmail.com>
2022-11-23 10:53:54 -0500

    fixed merge conflict and added toggle a password visibility  feature

--- client/features/events/Events.js
+++ client/features/events/Events.js
@@ -26,31 +26,20 @@ export default function Events() {
       {loading ? (
         <LoadingScreen />
       ) : (
-    <div className="container">
-      {isLoggedIn && <AddIcon />}
-      <h1 className="fw-light text-center text-lg-center p-4"> All Events </h1>
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-        {events?.map(({ id, image, title, start, tag }) => (
-          <div key={id}>
-            <div className="card text-center h-100">
-              <img
-                className="card-img-top "
-                src={image}
-              alt={`Image of ${title}`}
-                onClick={() => navigate(`/events/${id}`)}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {moment(start).format("dddd, MMMM Do, YYYY")}
-                </h6>
-                <small className="card-subtitle mb-2 text-muted">
-                  Tags: {tag.join(", ")}
-                </small>
-                <div>
-                  <Button
-                    size="small"
-
+        <div className="container">
+          {isLoggedIn && <AddIcon />}
+          <h1 className="fw-light text-center text-lg-center p-4">
+            {" "}
+            All Events{" "}
+          </h1>
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+            {events?.map(({ id, image, title, start, tag }) => (
+              <div key={id}>
+                <div className="card text-center h-100">
+                  <img
+                    className="card-img-top "
+                    src={image}
+                    alt={`Image of ${title}`}
                     onClick={() => navigate(`/events/${id}`)}
                   />
                   <div className="card-body">
@@ -65,37 +54,53 @@ export default function Events() {
                       <Button
                         size="small"
                         onClick={() => navigate(`/events/${id}`)}
-                      >
-                        More Info
-                      </Button>
-                    </div>
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">
+                          {moment(start).format("dddd, MMMM Do, YYYY")}
+                        </h6>
+                        <small className="card-subtitle mb-2 text-muted">
+                          Tags: {tag.join(", ")}
+                        </small>
+                        <div>
+                          {" "}
+                          <Button
+                            size="small"
+                            onClick={() => navigate(`/events/${id}`)}
+                          >
+                            More Info{" "}
+                          </Button>
+                        </div>
+                      </div>
+                    </div>{" "}
                   </div>
                 </div>
               </div>
             ))}
           </div>
-        ))}
-      </div>
-      <Tooltip title="Scroll to Top">
-        <Button
-          className="scroll"
-          onClick={() => {
-            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-          }}
-          variant="contained"
-          size="small"
-          style={{
-            position: "fixed",
-            borderRadius: "50%",
-            padding: "1rem 1rem",
-            bottom: "120px",
-            right: "10px",
-            textAlign: "center",
-          }}
-        >
-          <KeyboardArrowUpIcon></KeyboardArrowUpIcon>
-        </Button>
-      </Tooltip>
+          <Tooltip title="Scroll to Top">
+            <Button
+              className="scroll"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+              variant="contained"
+              size="small"
+              style={{
+                position: "fixed",
+                borderRadius: "50%",
+                padding: "1rem 1rem",
+                bottom: "120px",
+                right: "10px",
+                textAlign: "center",
+              }}
+            >
+              <KeyboardArrowUpIcon></KeyboardArrowUpIcon>
+            </Button>
+          </Tooltip>
+        </div>
+      )}
     </div>
   );
 }
