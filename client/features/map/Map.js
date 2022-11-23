bcaca2989ee954c700d828f1d36b0a6a0b5ede28
Noor Mudassar <noormudassar97@gmail.com>
2022-11-22 20:01:39 -0500

    cleaned up code for event pins, events clickable

--- client/features/map/Map.js
+++ client/features/map/Map.js
@@ -1,45 +1,3 @@
-// import React from "react";
-// import "leaflet/dist/leaflet.css";
-// import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-// import { icon } from "leaflet";
-
-// const ICON = icon({
-//   iconUrl: "/geo-fill.svg",
-//   iconSize: [29, 29],
-// });
-
-// function Map() {
-//   return (
-//     <div id="map" className="map, underline">
-//       <h1 align="center">Map</h1>
-//       <div className="d-flex justify-content-center">
-//         <MapContainer
-//           center={[40.6782, -73.9442]}
-//           zoom={11}
-//           scrollWheelZoom={true}
-//         >
-//           <TileLayer
-//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           />
-//           <div>
-//             <Marker
-//               icon={ICON}
-//               position={[40.70296058477033, -74.01551608783149]}
-//             >
-//               <Popup>
-//                 A testing popup. <br /> This is The Battery Park.
-//               </Popup>
-//             </Marker>
-//           </div>
-//         </MapContainer>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Map;
-
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -51,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 const ICON = icon({
   iconUrl: "/geo-fill.svg",
   iconSize: [29, 29],
+  className: "icon",
 });
 
 const Map = () => {
@@ -75,23 +34,31 @@ const Map = () => {
   return (
     <div id="map" className="map, underline">
       <h1 align="center">Map</h1>
-      <MapContainer center={[40.6782, -73.9442]} zoom={11}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {events.map((event) => {
-          return (
-            <Marker
-              icon={ICON}
-              key={event.id}
-              position={[event.latitude, event.longitude]}
-            >
-              <Popup>onClick={(ev) => navigateToEvent(ev, id)}</Popup>
-            </Marker>
-          );
-        })}
-      </MapContainer>
+      <div className="d-flex justify-content-center">
+        <MapContainer center={[40.6782, -73.9442]} zoom={11}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {events.map((event) => {
+            return (
+              <Marker
+                icon={ICON}
+                key={event.id}
+                position={[event.latitude, event.longitude]}
+              >
+                <Popup onClick={(ev) => navigateToEvent(ev, id)}>
+                  <Link to={`/events/${event.id}`}>
+                    {event.title} <br />
+                  </Link>
+                  {event.description}
+                </Popup>
+                {/* <button onClick={(ev) => navigateToEvent(ev, id)} /> */}
+              </Marker>
+            );
+          })}
+        </MapContainer>
+      </div>
     </div>
   );
 };
