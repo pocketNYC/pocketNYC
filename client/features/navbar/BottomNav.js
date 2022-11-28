cc472179420d9b1393c47cacb59315a58fc44b4a
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-11-28 12:07:15 -0500

    added color to icons

--- client/features/navbar/BottomNav.js
+++ client/features/navbar/BottomNav.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@mui/material/Avatar";
 import MapIcon from "@mui/icons-material/Map";
 import HomeIcon from "@mui/icons-material/Home";
 import EventAvailableIcon from "@mui/icons-material/EventAvailable";
-import InfoSharpIcon from "@mui/icons-material/InfoSharp";
 import AccountMenu from "./AccountMenu";
-
+import Diversity1Icon from "@mui/icons-material/Diversity1";
 import {
   BottomNavigation,
   Paper,
@@ -16,15 +14,6 @@ import {
 } from "@mui/material";
 
 export default function BottomNav() {
-  // const useStyles = makeStyles((theme) => ({
-  //   avatar: {
-  //     backgroundColor: theme.palette.grey[50],
-  //     border: `1px solid ${theme.palette.info.main}`,
-  //     color: theme.palette.info.main,
-  //   },
-  // }));
-  // const classes = useStyles();
-
   return (
     <Box sx={{ pb: 7 }}>
       <CssBaseline />
@@ -37,7 +26,7 @@ export default function BottomNav() {
             href="/home"
             label="Home"
             icon={
-              <Avatar>
+              <Avatar sx={{ backgroundColor: "#0039A6" }}>
                 <HomeIcon />
               </Avatar>
             }
@@ -45,15 +34,38 @@ export default function BottomNav() {
           <BottomNavigationAction
             label="Events"
             href="/events"
-            icon={<EventAvailableIcon />}
+            icon={
+              <Avatar sx={{ backgroundColor: "#FF6319" }}>
+                <EventAvailableIcon />
+              </Avatar>
+            }
           />
-          <BottomNavigationAction label="Map" href="/map" icon={<MapIcon />} />
+          <BottomNavigationAction
+            label="Map"
+            href="/map"
+            icon={
+              <Avatar sx={{ backgroundColor: "#808183" }}>
+                <MapIcon />
+              </Avatar>
+            }
+          />
           <BottomNavigationAction
             label="Resources"
             href="/resources"
-            icon={<InfoSharpIcon />}
+            icon={
+              <Avatar sx={{ backgroundColor: "#EE352E" }}>
+                <Diversity1Icon />
+              </Avatar>
+            }
           />
-          <BottomNavigationAction label="User" icon={<AccountMenu />} />
+          <BottomNavigationAction
+            label="User"
+            icon={
+              <Avatar sx={{ backgroundColor: "#00933C" }}>
+                <AccountMenu />
+              </Avatar>
+            }
+          />
         </BottomNavigation>
       </Paper>
     </Box>
